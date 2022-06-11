4ad3de8a2b4c7bfa6899ee224e7ce65f7b157e02
jakzaizzat <jakzaizzat@gmail.com>
2022-06-11 11:18:28 +0800

    Update collection traits

--- constants/collections.ts
+++ constants/collections.ts
@@ -8,6 +8,36 @@ export const COLLECTIONS = [
       'https://openseauserdata.com/files/a24a150c98775325ff95e0d563ff96f2.mp4#t=0.001',
     imageUrl:
       'https://lh3.googleusercontent.com/ZC9bdwv0YBDw14fa1bOGWlkDjczYN68dBFWhYfnua_VMAGchzlnU6NlJok7RYBr5WyZdf9HOh13Wl35RXW_g4fB4a572GzXHdri0',
+    attributes: [
+      {
+        trait_type: 'Animation',
+        value: 'Normal walk',
+      },
+      {
+        trait_type: 'Attire',
+        value: 'Normal',
+      },
+      {
+        trait_type: 'Background',
+        value: 'Veronoi',
+      },
+      {
+        trait_type: 'Backpack',
+        value: 'Odeng',
+      },
+      {
+        trait_type: 'Flying Object',
+        value: 'Odeng Emoji',
+      },
+      {
+        trait_type: 'Head',
+        value: 'Horned',
+      },
+      {
+        trait_type: 'Shoe',
+        value: 'Balenciagar',
+      },
+    ],
   },
   {
     id: '1',
@@ -18,6 +48,32 @@ export const COLLECTIONS = [
       "Do you want a business card? I'm your card creator. The card gives your identity to an object.",
     videoUrl:
       'https://openseauserdata.com/files/8bfab1a2f34b6e671259dc4c547ce144.mp4#t=0.001',
+    attributes: [
+      {
+        trait_type: 'Animation',
+        value: 'Normal walk',
+      },
+      {
+        trait_type: 'Attire',
+        value: 'Shirt Only',
+      },
+      {
+        trait_type: 'Background',
+        value: 'Veronoi',
+      },
+      {
+        trait_type: 'Backpack',
+        value: 'Card',
+      },
+      {
+        trait_type: 'Head',
+        value: 'Card',
+      },
+      {
+        trait_type: 'Shoe',
+        value: 'Nikee Sneakers',
+      },
+    ],
   },
   {
     id: '2',
@@ -28,26 +84,165 @@ export const COLLECTIONS = [
       'NFXT is a community hub that encourages creative exploration through learning and play. NFXT in September 2021 just minted so many NFXT tokens that the coin itself just dropped from the coin bag. It has discord and hope that you yes you on board in the journey of creative exploration',
     imageUrl:
       'https://lh3.googleusercontent.com/EWh9LCLaB3LnIxDcy25GEnmvTd2LSPZKTkY7QjuqGzU42ShwQ1RVH_denMSDtaJdNdOtwo8Ed9cRX4le04WUhhxPYp3Y3VLOjodoOLQ',
+    attributes: [
+      {
+        trait_type: 'Animation',
+        value: 'Walk And Typing',
+      },
+      {
+        trait_type: 'Attire',
+        value: 'Shirt Only',
+      },
+      {
+        trait_type: 'Background',
+        value: 'Veronoi',
+      },
+      {
+        trait_type: 'Backpack',
+        value: 'NFXT Coin Bag',
+      },
+      {
+        trait_type: 'Flying Object',
+        value: 'Hologram Discord',
+      },
+      {
+        trait_type: 'Head',
+        value: 'NFXT Token',
+      },
+      {
+        trait_type: 'Shoe',
+        value: 'Nikee Air',
+      },
+    ],
   },
   {
     id: '3',
     name: 'Zeeraffe the Voxelteer',
-    videoUrl: 'https://openseauserdata.com/files/95918417c9db1a565b7f18c35d7b3bff.mp4#t=0.001',
-    description: 'In lyfe , we need to electrify things to charge up and when it full , It is ready to go to its best .',
-    imageUrl: 'https://lh3.googleusercontent.com/iNOTlBSFisWj4ru8vKdaJCfh-cB9hV5euawsoUPn_XUpluSgB9CtRlEsLdQWxDtvJhwvTstpy_pOYfpfdEi73IqX8sbSsaHMSVY7Cw',
+    videoUrl:
+      'https://openseauserdata.com/files/95918417c9db1a565b7f18c35d7b3bff.mp4#t=0.001',
+    description:
+      'In lyfe , we need to electrify things to charge up and when it full , It is ready to go to its best .',
+    imageUrl:
+      'https://lh3.googleusercontent.com/iNOTlBSFisWj4ru8vKdaJCfh-cB9hV5euawsoUPn_XUpluSgB9CtRlEsLdQWxDtvJhwvTstpy_pOYfpfdEi73IqX8sbSsaHMSVY7Cw',
+    attributes: [
+      {
+        trait_type: 'Animation',
+        value: 'Normal Walk',
+      },
+      {
+        trait_type: 'Attire',
+        value: 'Normal',
+      },
+      {
+        trait_type: 'Background',
+        value: 'Veronoi',
+      },
+      {
+        trait_type: 'Effects',
+        value: 'Electric Particle',
+      },
+      {
+        trait_type: 'Flying Object',
+        value: 'Nouns Left Sided',
+      },
+      {
+        trait_type: 'Flying Object',
+        value: 'Aircraft',
+      },
+      {
+        trait_type: 'Head',
+        value: 'Electric Artifact',
+      },
+      {
+        trait_type: 'Shoe',
+        value: 'Balenciagar',
+      },
+    ],
   },
   {
     id: '4',
     name: 'Vuevossa the Balloon Puppeteer',
-    videoUrl: 'https://openseauserdata.com/files/b570f4fe0c7dc71df12831659f034588.mp4#t=0.001',
-    description: 'Bubble is unique. Bubble has colors in its transparency. Bubble is fragile. What important is that bubble give you smile.',
-    imageUrl: 'https://lh3.googleusercontent.com/K_Peq1SGg20PLEML_3B2-QIWsfqVzbt5lUxk6NrSkTOUnzhRJ9878ufPDtuHVrnqtaCk3gd7kfx_t5_ohXS5KqeWr1vxQnVwdnIyjA',
+    videoUrl:
+      'https://openseauserdata.com/files/b570f4fe0c7dc71df12831659f034588.mp4#t=0.001',
+    description:
+      'Bubble is unique. Bubble has colors in its transparency. Bubble is fragile. What important is that bubble give you smile.',
+    imageUrl:
+      'https://lh3.googleusercontent.com/K_Peq1SGg20PLEML_3B2-QIWsfqVzbt5lUxk6NrSkTOUnzhRJ9878ufPDtuHVrnqtaCk3gd7kfx_t5_ohXS5KqeWr1vxQnVwdnIyjA',
+
+    attributes: [
+      {
+        trait_type: 'Animation',
+        value: 'Normal Walk',
+      },
+      {
+        trait_type: 'Attire',
+        value: 'Musgrave',
+      },
+      {
+        trait_type: 'Background',
+        value: 'Veronoi',
+      },
+      {
+        trait_type: 'Backpack',
+        value: 'Bubble Bottle',
+      },
+      {
+        trait_type: 'Effects',
+        value: 'Bubble Particle',
+      },
+      {
+        trait_type: 'Flying Object',
+        value: 'Bubble',
+      },
+      {
+        trait_type: 'Head',
+        value: 'Bubble Artifact',
+      },
+      {
+        trait_type: 'Shoe',
+        value: 'Nikee Air',
+      },
+    ],
   },
   {
     id: '5',
     name: 'Mulo x Nice to meet you',
-    videoUrl: 'https://openseauserdata.com/files/6c5346a4d0f2c353b7d29a59b2aa6d82.mp4#t=0.001',
-    description: 'Have you greeted people lately? Do you find it hard? If hard right, why not start with "You, Nice to meet you".',
-    imageUrl: 'https://lh3.googleusercontent.com/nwK1oV1zyCqa1C9t-gtEShWHJ3vzAriuA_NY3tD54ljzMlyltcjjSPB9vGlbAXj2qvdI3TozfEXoc6gU2frsDNm_29jCxUPhrSZcpA',
-  }
+    videoUrl:
+      'https://openseauserdata.com/files/6c5346a4d0f2c353b7d29a59b2aa6d82.mp4#t=0.001',
+    description:
+      'Have you greeted people lately? Do you find it hard? If hard right, why not start with "You, Nice to meet you".',
+    imageUrl:
+      'https://lh3.googleusercontent.com/nwK1oV1zyCqa1C9t-gtEShWHJ3vzAriuA_NY3tD54ljzMlyltcjjSPB9vGlbAXj2qvdI3TozfEXoc6gU2frsDNm_29jCxUPhrSZcpA',
+
+    attributes: [
+      {
+        trait_type: 'Animation',
+        value: 'Normal Walk',
+      },
+      {
+        trait_type: 'Attire',
+        value: 'Normal',
+      },
+      {
+        trait_type: 'Background',
+        value: 'Veronoi',
+      },
+      {
+        trait_type: 'Effects',
+        value: 'Drawing Stopmotion',
+      },
+      {
+        trait_type: 'Effects',
+        value: 'Electric Particle',
+      },
+      {
+        trait_type: 'Head',
+        value: 'Mulo',
+      },
+      {
+        trait_type: 'Shoe',
+        value: 'Balenciagar',
+      },
+    ],
+  },
 ]
